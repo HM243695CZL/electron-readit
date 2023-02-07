1cee04f9a4c35c0c112de6aa1735abdb7c40fa3e
hm243695czl <1224311695@qq.com>
2023-02-07 22:37:16 +0800

    项目基本架构搭建

---
+++ main.ts
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } =  require('electron');
+const WinState = require('electron-win-state').default;
+const path = require('path');
+
+
+const createWindow = () => {
+    const winState = new WinState({
+       defaultWidth: 1000,
+       defaultHeight: 800
+    });
+    const win = new BrowserWindow({
+        ...winState.winOptions,
+        webPreferences: {
+            preload: path.resolve(__dirname, './preload/index.ts')
+        }
+    });
+    win.loadURL("http://localhost:5173");
+
+    win.webContents.openDevTools();
+    winState.manage(win);
+}
+
+app.whenReady().then(() => {
+    createWindow();
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+});
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+})
