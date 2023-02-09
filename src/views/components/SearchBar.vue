c86e96431a97a530d99b284e9db80f3f4862743d
hm243695czl <1224311695@qq.com>
2023-02-09 22:38:06 +0800

    添加搜索栏

--- src/views/components/SearchBar.vue
+++ src/views/components/SearchBar.vue
@@ -1,14 +1,12 @@
 <template>
-  <div class="search-bar-container">
-    <div class="search-container">
-      <div class="button" @click="handleClick">+</div>
-      <div class="input">
-        <input
-            type="text"
-            placeholder="请输入关键字..."
-            @keyup="search"
-        >
-      </div>
+  <div class="search-container">
+    <div class="button" @click="handleClick">+</div>
+    <div class="input">
+      <input
+          type="text"
+          placeholder="请输入关键字..."
+          @keyup="search"
+      >
     </div>
   </div>
 </template>
@@ -21,35 +19,38 @@ export default defineComponent({
 })
 </script>
 
-<style scoped lang="stylus">
-  .search-bar-container
-    div.search-container
-      height 60px
-      background #d3d3d3
-      display flex
-      align-items center
-      justify-content center
-      padding-left 10px
-      .button
-        width 40px
-        height 40px
-        background-color #1e90ff
-        padding-left 0
-        border-radius 5px
-        font-size 30px
-        font-weight bold
-        color #fff
-        text-align center
-        cursor default
-      .input
-        flex 1
-        height 36px
-        padding 0 10px
-        input
-          height 100%
-          width 100%
-          outline none
-          border solid 1px #ccc
-          border-radius 5px
-          padding 0 10px
+<style lang="less">
+  .search-container{
+    height: 60px;
+    background: #d3d3d3;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding-left: 10px;
+    .button{
+      width: 40px;
+      height: 40px;
+      background-color: #1e90ff;
+      padding-left: 0;
+      border-radius: 5px;
+      font-size: 30px;
+      font-weight: bold;
+      color: #fff;
+      text-align: center;
+      cursor: default;
+    }
+    .input{
+      flex: 1;
+      height: 36px;
+      padding: 0 10px;
+      input{
+        height: 100%;
+        width: 100%;
+        outline: none;
+        border: 1px solid #ccc;
+        border-radius: 5px;
+        padding: 0 10px;
+      }
+    }
+  }
 </style>
